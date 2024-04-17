52cf32281b308ddbc4f0c379c420f2e12b09b81a
Dongcan Pang <pangdongcan@live.com>
2024-04-17 19:15:23 +0800

    feature-demo-4-multiThread: some test codes about Web Workers API (Multi-Thread)

--- main.js
+++ main.js
@@ -1,74 +1,21 @@
-const {app, BrowserWindow, Menu, MenuItem, Notification, globalShortcut} = require('electron')
+const {app, BrowserWindow} = require('electron')
 
 //窗口创建函数
 const createWindow = () => {
     //主窗口
     const win = new BrowserWindow({
         width:1000,
-        height:600
+        height:600,
+        webPreferences:{
+            //可以在 Electron 的 Web Workers 里使用 Node.js的特性。
+            //要用的话，需把 webPreferences 中的 nodeIntegrationInWorker 选项设置为 true
+            nodeIntegrationInWorker:true
+        }
     });
     //加载页面
     win.loadFile('index.html');
     //打开开发者工具
     win.webContents.openDevTools();
-
-    //在调度页面中的keydown和keyup事件之前，会发出before-input-event事件。 它可以用于捕获和处理在菜单中不可见的自定义快捷方式。
-    win.webContents.on('before-input-event', (event, input)=>{
-        if(input.control && input.key.toLowerCase()==='i'){
-            console.log("你按下了 Ctrl + I ");
-            event.preventDefault();
-        }
-    })
-}
-
-//创建一个自定义的菜单（设置菜单的同时，设置本地快捷键）
-const menu = new Menu()
-menu.append(new MenuItem({
-    label:"Electron",
-    submenu:[
-        {
-            role:'help',
-            accelerator: process.platform==='darwin'? 'Alt+Cmd+I' : 'Alt+Shift+I',
-            click:()=>{
-                //如果支持 Notification 则提示，否则将在后台打印
-                if(Notification.isSupported){
-                    let myAlert = new Notification({
-                        title: app.name,
-                        body : "本地快捷键触发了。。。"
-                    })
-                    myAlert.show();
-                } else {
-                    console.log('本地快捷键触发了。。。');
-                }
-            }
-        }
-    ]
-}))
-//添加到应用的菜单栏
-Menu.setApplicationMenu(menu)
-
-//设置一个函数，用于注册全局快捷键
-function myGlobalHotkeys(){
-    //注册快捷键 Alt + CmdOrCtrl + P
-    let register1 = globalShortcut.register("Alt+CommandOrControl+P", ()=>{
-        //如果支持 Notification 则提示，否则将在后台打印
-        if(Notification.isSupported){
-            let myAlert = new Notification({
-                title: app.name,
-                body : "全局快捷键触发了。。。"
-            })
-            myAlert.show();
-        } else {
-            console.log('全局快捷键触发了。。。');
-        }
-    })
-    if(!register1){
-        console.log("全局快捷键 register1 注册失败");
-    }else if(!globalShortcut.isRegistered("Alt+CommandOrControl+P")){
-        console.log("全局快捷键 register1 注册失败，快捷键已经被其他应用程序注册");
-    }else{
-        console.log("全局快捷键 register1 注册成功");
-    }
 }
 
 //设置一个主函数
@@ -85,29 +32,16 @@ async function main(){
     }
 
     await app.whenReady().then(()=>{
-
-        //注册一个该应用的全局快捷键
-        myGlobalHotkeys();
-
         //创建主窗口
         createWindow();
-
         //窗口全部关闭事件
         app.on('window-all-closed', ()=>{
             //如果用户不是在 macOS(darwin) 上运行程序，则调用 app.quit()。
             if(process.platform !== 'darwin') app.quit();
         });
-        
         app.on('activate', ()=>{
             if(BrowserWindow.getAllWindows().length()===0) createWindow()
         });
-
-        //当app即将退出，取消全局快捷键的注册
-        app.on('will-quit', ()=>{
-            //清空该应用程序的所有全局快捷键
-            globalShortcut.unregisterAll();
-        })
-        
     })
 }
 
---
+++ worker1.js
@@ -0,0 +1,8 @@
+onmessage = (e) => {
+    //输出接收到的信息
+    console.log('我是 worker-1, 我收到了主进程发来的信息：'+e.data)
+    let randomStr = Math.random()+""
+    randomStr = randomStr.substring(randomStr.length-4)
+    //发送一个消息给主进程。。。
+    postMessage("你好，我是worker-1，有什么可以帮到您？"+randomStr)
+}
