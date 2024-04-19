3f280598f27ca55391f7e2992b595b6c88502cf9
Dongcan Pang <pangdongcan@live.com>
2024-04-19 17:11:39 +0800

    feature: organize some codes of demo-9.

---
+++ demo-9-iframe/main.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow} = require('electron')
+
+//窗口创建函数
+const createWindow = () => {
+    //主窗口
+    const win = new BrowserWindow({
+        width:1000,
+        height:600
+    });
+    //加载页面
+    win.loadFile('index.html');
+}
+
+//设置一个主函数
+async function main(){
+
+    //启用全局沙盒化，安全设置
+    app.enableSandbox();
+
+    //Squirrel 在 程序在 安装、更新、卸载等阶段，会通过调起主程序的方式通知到主程序，
+    //我们要把这些启动方式和用户主动打开的方式区别开来。
+    if(require('electron-squirrel-startup')){
+        app.quit()
+        return ;
+    }
+
+    await app.whenReady().then(()=>{
+        //创建主窗口
+        createWindow();
+        //窗口全部关闭事件
+        app.on('window-all-closed', ()=>{
+            //如果用户不是在 macOS(darwin) 上运行程序，则调用 app.quit()。
+            if(process.platform !== 'darwin') app.quit();
+        });
+        app.on('activate', ()=>{
+            if(BrowserWindow.getAllWindows().length()===0) createWindow()
+        });
+    })
+}
+
+//主程序启动
+main().catch(console.error)
