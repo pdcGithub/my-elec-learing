088691806b2732caa5919dd3ad50b0e18c2f642f
Dongcan Pang <pangdongcan@live.com>
2024-04-20 14:40:50 +0800

    feature-demo-11-taskbar: test some features of demo-11.

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const {app, BrowserWindow, WebContentsView} = require('electron')
+const {app, BrowserWindow, Notification} = require('electron')
+const path = require('node:path')
+
+//增加一个函数，在主进程调用消息提示
+function showNotification(message){
+    //如果支持 Notification 则提示，否则将在后台打印
+    if(Notification.isSupported){
+        new Notification({title: app.name, body : message}).show();
+    } else {
+        console.log("你的操作系统不支持 Notification 消息提示，所以消息显示在后台，如下："+message);
+    }
+}
 
 //窗口创建函数
 const createWindow = () => {
@@ -6,24 +17,31 @@ const createWindow = () => {
     //主窗口
     const win = new BrowserWindow({
         width:1000,
-        height:600,
-        backgroundColor: '#efefef' /*将窗口的背景设置为 灰色，以便观察边框*/
+        height:600
     });
 
-    //加载子页面 view1 BrowserView 类已弃用，并替换为新的 WebContentsView 类。Electron v30.0.1
-    const view1 = new WebContentsView();
-    win.contentView.addChildView(view1);
-    view1.webContents.loadURL('https://electronjs.org');
-    view1.setBounds({ x: 0, y: 0, width: 400, height: 500 });
+    //设置缩略图工具栏（鼠标悬停在任务栏的图标上，即可看见）
+    win.setThumbarButtons([
+        {
+            icon: path.join(__dirname, 'left.png'),
+            tooltip:'向左',
+            click : () => {showNotification('你点击了，左键')}
+        },
+        {
+            icon: path.join(__dirname, 'right.png'),
+            tooltip:'向右',
+            click : () => {showNotification('你点击了，右键')}
+        }
+    ]);
 
-    //加载子页面 view2 BrowserView 类已弃用，并替换为新的 WebContentsView 类。Electron v30.0.1
-    const view2 = new WebContentsView();
-    win.contentView.addChildView(view2);
-    view2.webContents.loadURL('https://www.mickarea.net'); /* 这是我的个人网站，有兴趣可以看看 */
-    view2.setBounds({ x: 450, y: 0, width: 500, height: 500 });
+    //任务栏中的图标叠加
+    win.setOverlayIcon(path.join(__dirname, 'circle.png'), '这是一段描述信息...')
 
-    //打开开发者工具
-    //win.webContents.openDevTools();
+    //任务栏中的图标闪烁（windows下，效果不是很明显）
+    win.flashFrame(true);
+    setTimeout(()=>{
+        win.flashFrame(false);/* 5秒后停止闪烁 */
+    }, 5000);
 }
 
 //设置一个主函数
@@ -31,6 +49,26 @@ async function main(){
 
     //启用全局沙盒化，安全设置
     app.enableSandbox();
+
+    //设置任务列表（在任务栏的图标上，右键点击即可看见）
+    app.setUserTasks([
+        {
+            program  : process.execPath,
+            arguments: '--new-window',
+            iconPath : process.execPath,
+            iconIndex: 0,
+            title    : '任务1',
+            description: '创建一个新窗口'
+        },
+        {
+            program  : process.execPath,
+            arguments: '--new-window',
+            iconPath : process.execPath,
+            iconIndex: 0,
+            title    : '任务2',
+            description: '创建一个新窗口'
+        }
+    ]);
 
     //Squirrel 在 程序在 安装、更新、卸载等阶段，会通过调起主程序的方式通知到主程序，
     //我们要把这些启动方式和用户主动打开的方式区别开来。
